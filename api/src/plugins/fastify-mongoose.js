e16d01faf1e4b6231f0db62885d6e2c62388e36b
Aid Arslanagic <aarsla@gmail.com>
2020-03-10 11:56:53 +0100

    favicon

--- api/src/plugins/fastify-mongoose.js
+++ api/src/plugins/fastify-mongoose.js
@@ -14,6 +14,7 @@ async function mongooseConnector (fastify, options, next) {
   }
 
   mongoose.Promise = global.Promise
+  // mongoose.set('debug', true)
 
   await mongoose.connect(uri, mongodbOptions)
 
